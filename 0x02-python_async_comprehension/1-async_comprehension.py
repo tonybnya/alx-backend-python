66bf43e6925712fae24cfec2b01ac69f8d1a1359
Tony <nya.tony2010@gmail.com>
2023-10-10 16:21:33 +0100

    Add: '1-async_comprehension.py' file

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Async Comprehensions
+"""
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Coroutine
+    """
+    return [num async for num in async_generator()]
