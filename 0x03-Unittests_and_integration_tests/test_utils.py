7a7a69fc45c091a6e18f43d8fe204223f4f0a403
Tony <nya.tony2010@gmail.com>
2023-10-30 17:19:04 +0100

    Update: 'test_utils.py' file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,7 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
         [
             ({"a": 1}, ("a",), 1),
             ({"a": {"b": 2}}, ("a",), {"b", 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
         ]
     )
     def test_access_nested_map(self, nested_map, path, expected):
